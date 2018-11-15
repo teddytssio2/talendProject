793186a7417a252f22979373e489070f1c7e0a81
Teddy LEQUETTE <Teddy.LEQUETTE@hardis.fr>
2018-11-15 12:09:25 +0100

    Tenth

---
+++ jobs/process/copy_of_justajob3_1.2/src/main/templates/Job_run_template.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd `dirname $0`
+ROOT_PATH=`pwd`
+java -Xms256M -Xmx1024M -cp .:$ROOT_PATH:$ROOT_PATH/../lib/routines.jar:$ROOT_PATH/../lib/antlr-runtime-3.5.2.jar:$ROOT_PATH/../lib/dom4j-1.6.1.jar:$ROOT_PATH/../lib/log4j-1.2.16.jar:$ROOT_PATH/../lib/org.talend.dataquality.parser.jar:$ROOT_PATH/../lib/talend_file_enhanced_20070724.jar:$ROOT_PATH/copy_of_justajob3_1_2.jar: projet3.copy_of_justajob3_1_2.Copy_of_JustAJob3  --context=Default "$@" 
