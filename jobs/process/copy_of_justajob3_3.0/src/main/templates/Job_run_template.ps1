a0bf77ad542d17c22a46f3cf5890a13471d17c1e
Teddy LEQUETTE <Teddy.LEQUETTE@hardis.fr>
2018-11-15 12:03:16 +0100

    Nineth

--- jobs/process/copy_of_justajob3_3.0/src/main/templates/Job_run_template.ps1
+++
@@ -1,3 +0,0 @@
-$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-cd $fileDir
-java '-Xms256M' '-Xmx1024M' -cp '.;../lib/routines.jar;../lib/antlr-runtime-3.5.2.jar;../lib/dom4j-1.6.1.jar;../lib/log4j-1.2.16.jar;../lib/org.talend.dataquality.parser.jar;../lib/talend_file_enhanced_20070724.jar;copy_of_justajob3_3_0.jar;' projet3.copy_of_justajob3_3_0.Copy_of_JustAJob3  --context=Default %* 
